165f0ac9a4d682f387b022127be6a0b590d80a0b
LakshmiNarsitha <79990816+LakshmiNarsitha@users.noreply.github.com>
2022-11-14 21:52:17 +0530

    Update merge_k_sortedlists.c

--- merge_k_sortedlists.c
+++ merge_k_sortedlists.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include<stdlib.h>
 struct ListNode {
   int val;
   struct ListNode *next;
@@ -38,4 +40,68 @@ struct ListNode* mergeKLists(struct ListNode** lists, int listsSize)
         result=mergeTwoLists(result,lists[i]);
     }
     return result;
+}
+struct ListNode* create(struct ListNode* first)
+{
+    struct ListNode *new,*prev;
+    int x;
+    printf("enter data,enter -1 to stop");
+    scanf("%d",&x);
+    while(x!=-1)
+    {
+        new=(struct ListNode*)malloc(sizeof(struct ListNode));
+        new ->val=x;
+        new ->next=NULL;
+        if(first==NULL)
+        {
+            first=new;
+            prev=new;
+        }
+        else
+        {
+            prev ->next=new;
+            prev=new;
+        }
+        printf("enter data");
+        scanf("%d",&x);
+    }
+    return first;
+}
+void display(struct ListNode *first)
+{
+    struct ListNode *temp=first;
+    if(first==NULL)
+    {
+        printf("[]");
+    }
+    else
+    {
+        printf("[");
+        while(temp!=NULL)
+        {
+            printf("%d ,",temp ->val);
+            temp = temp ->next;
+        }
+        printf("]");
+    }
+}
+void main()
+{
+    struct ListNode *first=NULL,*new,*res;
+    int n,i;
+    printf("enter no of lists : ");
+    scanf("%d",&n);
+    struct ListNode*lists[n];
+    for(i=0;i<n;i++)
+    {
+        lists[i]=(struct ListNode*)(malloc(sizeof(struct ListNode)));
+
+    }
+    for(i=0;i<n;i++)
+    {
+         new=(struct ListNode*)(malloc(sizeof(struct ListNode)));
+         lists[i]=create(first);
+    } 
+    res=mergeKLists(lists,n);
+    display(res);
 }
